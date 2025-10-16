4cd667ec853b27157c93fa59147e40080c601f60
Tangi Beneat <tangi.beneat@gmail.com>
2025-10-16 09:26:47 +0200

    add rubble in room 5

--- src/app/room-5/room-5.ts
+++ src/app/room-5/room-5.ts
@@ -2,6 +2,7 @@ import { Component, ViewChild, ViewContainerRef } from '@angular/core';
 import { Room5Bed } from './room-5-bed/room5-bed';
 import { Room5Case } from './room-5-case/room-5-case';
 import { Room5Shelf } from './room-5-shelf/room5-shelf';
+import { Room5Rubble } from './room5-rubble/room5-rubble';
 
 enum Props {
   ROOM5_CASE = "la vielle caisse en bois",
@@ -20,22 +21,23 @@ export class Room5 {
   @ViewChild('inspected_props_element', { read: ViewContainerRef, static: true })
   container!: ViewContainerRef;
 
-  protected props = [Props.ROOM5_BED, Props.ROOM5_CASE, Props.ROOM5_SHELF];
+  protected props = [Props.ROOM5_BED, Props.ROOM5_CASE, Props.ROOM5_SHELF, Props.ROOM5_RUBBLE];
   protected inspected_props: any;
 
   inspect(prop: Props) {
+    this.container.clear();
     switch (prop) {
       case Props.ROOM5_BED:
-        this.container.clear();
         this.container.createComponent(Room5Bed);
         break;
       case Props.ROOM5_CASE:
-        this.container.clear();
         this.container.createComponent(Room5Case);
         break;
       case Props.ROOM5_SHELF:
-        this.container.clear();
         this.container.createComponent(Room5Shelf);
+        break;
+      case Props.ROOM5_RUBBLE:
+        this.container.createComponent(Room5Rubble);
         break;
     }
   }
---
+++ src/app/room-5/room5-rubble/room5-rubble.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-room5-rubble',
+  imports: [],
+  templateUrl: './room5-rubble.html',
+  styleUrl: './room5-rubble.css'
+})
+export class Room5Rubble {
+
+}
